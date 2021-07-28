462eb90319c1f0911a92ead9f53b861785171574
Busaqui <78212243+busaqui-app@users.noreply.github.com>
2021-07-28 00:16:55 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import MainStack from './src/navigators/MainStack';
+import { store, persistor } from './src/services/store';
+import Route280 from './src/screens/Route280/index';
+import Home from './src/screens/Home';
+
+export default () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>  
+      {/* <MainStack/> */}
+      <Home/>
+      {/* <Route280 />  */}
+    </PersistGate>{/*Fim PersistGate */}
+  </Provider>/*Fim Provider*/
+);/*Fim Export default*/
+
